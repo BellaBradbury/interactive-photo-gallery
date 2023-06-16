6e0e738b2c20bfaba087562a453bfb4772113c6e
BellaBradbury <bellabradbury@yahoo.com>
2023-06-16 11:34:43 -0400

    modal btns *

--- js/app.js
+++ js/app.js
@@ -2,6 +2,45 @@
 baguetteBox.run('.gallery');
 
 
+// ---------- BUTTON LIMIT FUNCTIONALITY ---------- //
+const galleryCot = document.getElementsByClassName('gallery')[0];
+const modalOverlay = document.getElementById('baguetteBox-overlay');
+const photoModal = document.getElementById('baguetteBox-slider');
+const modalBtns = Array.from( modalOverlay.getElementsByTagName('button') );
+
+const backBtn = document.getElementById('previous-button');
+const nextBtn = document.getElementById('next-button');
+
+function hideNav() {
+    let modalCheck = photoModal.style.transform;
+    console.log(modalCheck);
+
+    if (modalCheck === 'translate3d(0%, 0px, 0px)') {
+        backBtn.style.display = 'none';
+    } else if (modalCheck === 'translate3d(-1100%, 0px, 0px)') {
+        nextBtn.style.display = 'none';
+    } else {
+        backBtn.style.display = 'inline-block';
+        nextBtn.style.display = 'inline-block';
+    }
+}
+
+galleryCot.addEventListener('click', hideNav);
+galleryCot.addEventListener('keyup', (e)=> {
+    if (e.key === 'Enter') {
+        hideNav();
+    }
+});
+
+modalBtns.forEach(btn => {
+    btn.addEventListener('click', hideNav);
+    btn.addEventListener('keyup', (e)=> {
+        if (e.key === 'Enter') {
+            hideNav();
+        }
+    });
+});
+
 // ---------- SEARCH BAR FUNCTIONALITY ---------- //
 const input = document.getElementById('search');
 const empty = document.getElementById('no-results');
